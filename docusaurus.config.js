7ba7f08ac660a9ea89c8d0a7f0386d1e74f258b9
Mario Frei <mario.frei@gmx.net>
2023-01-05 09:08:03 +0800

    Fix 'edit this page' button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,13 +97,13 @@ module.exports = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/FedericoTartarini/cozie-apple/tree/master/my-website",
+            "https://github.com/cozie-app/cozie-apple-website/tree/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/FedericoTartarini/cozie-apple/tree/master/my-website/blog",
+            "https://github.com/cozie-app/cozie-apple-website/tree/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
